67e3542cec0c11362765e8aa3cc8011ce1f95211
rafaelcarvalho64 <rafaeldecarvalhohs@gmail.com>
2023-03-28 11:21:05 -0300

    path  fix

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/contatos" element={<Home />} />
         <Route path="/" element={<Home />} />
+        <Route path="/crud-contatos-supabase-react" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
